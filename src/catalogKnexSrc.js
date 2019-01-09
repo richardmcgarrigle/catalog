f6273e40a9f17bf22dab86774ab8856d7e75d16d
Richard McGarrigle <youexample.com>
2019-01-09 07:46:48 +0000

    add getMatched()

--- src/catalogKnexSrc.js
+++ src/catalogKnexSrc.js
@@ -1,5 +1,6 @@
 "use strict"
 const tracer = require('tracer').console()
+const validator = require('validator')
 
 class catalogKnexSrc {
     constructor(knex){
@@ -8,6 +9,38 @@ class catalogKnexSrc {
 
     getAll(){
         return this.knex('catalog').select('name', 'description')
+        .catch((error)=>{
+            return this._defaultErrorHandling(error)
+        })
+    }
+
+    getMatched(filter){
+        /*
+        expecting array of strings
+        */
+        return new Promise((resolve, reject)=>{
+            let message = ''
+            if(filter.length > 0){
+                try{
+                    escapedFilter = JSON.parse(validator.escape(JSON.stringify))
+                    resolve(escapedFilter)
+                }
+                catch(error){
+                    tracer.info(error.name, error.description)
+                    message = 'invalid filter'
+                }
+                
+            }
+            else{
+                message('no filter provided')
+            }
+
+            reject(new Error(message))
+        
+        })
+        .then((escapedFilter)=>{
+            return this.knex('catalog').select('name', 'description').whereIn('description', escapedFilter)
+        })
         .catch((error)=>{
             return this._defaultErrorHandling(error)
         })
