72ffb289a74c8559f1931e5d3c304eb4426e7ace
Richard McGarrigle <youexample.com>
2019-01-09 07:56:16 +0000

    add post, put and delete methods

--- routes/catalog.knex.js
+++ routes/catalog.knex.js
@@ -21,4 +21,28 @@ router.get('/match', (req, res)=>{
     })
 })
 
+router.post('/', (req, res)=>{
+    // TODO: authentication steps to ensure user has permissions to add new items
+    catalogKnexSrc.addNew(req.query.catalogItems)
+    .then((collection)=>{
+        res.json(collection)
+    })
+})
+
+router.put('/', (req, res)=>{
+    // TODO: authentication steps to ensure user has permissions to update items
+    catalogKnexSrc.updateItem(req.query.catalogItems)
+    .then((collection)=>{
+        res.json(collection)
+    })
+})
+
+router.delete('/', (req, res)=>{
+    // TODO: authentication steps to ensure user has permissions to delete items
+    catalogKnexSrc.deleteItem(req.query.catalogItems)
+    .then((collection)=>{
+        res.json(collection)
+    })
+})
+
 module.exports = router;
